b3f2a4e2998f63eb91d51e8735da0d21ff48e84c
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-23 02:04:48 +0600

    [ADD] footer part

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
+import { footerLinks } from '@/constants'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Footer() {
@@ -9,7 +11,18 @@ function Footer() {
 					<Image src="/logo.svg" alt='logo' width={118} height={18} className='object-contain ' />
 					<p className="text-base text-gray-700 "> carHub 2023 <br />All rights reserved &copy; </p>
 				</div>
+				<div className="flex flex-wrap flex-1 w-full gap-20 md:justify-end max-md:mt-10">
+					{footerLinks?.map((link) => (
+						<div key={link.title} className="flex flex-col gap-6 text-base min-w-[170px]">
+							<h3 className="font-bold ">{link.title}</h3>
+							{link?.links?.map((item) => (
+								<Link href={item.url} key={item.title} className='text-gray-500 ' >{item.title}</Link>
+							))}
+						</div>
+					))}
+				</div>
 			</div>
+
 		</footer>
 	)
 }
