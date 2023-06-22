8c143af248a84d810bab1cf4da145d113b42d6c0
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-23 00:27:37 +0600

    [ADD] Hero heading and sub tittle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,16 @@
 
 function Hero() {
 	return (
-		<div>Hero</div>
+		<div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
+			<div className="flex-1 px-6 pt-36 sm:px-16">
+				<h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
+					Find , Book or rent a Car Quickly and easily!
+				</h1>
+				<h1 className="text-[27px] text-black-100 font-light mt-5">
+					Streamline Your car rental experience With our Effortless Booking Process.
+				</h1>
+			</div>
+		</div>
 	)
 }
 
