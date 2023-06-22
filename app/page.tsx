b34eab922cea1554e08642101d2f9fdd74d4de03
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-23 02:58:07 +0600

    [ADD] Search bar and custom filter added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from "@/components";
+import { Hero, SearchBar } from "@/components";
 
 
 
@@ -6,6 +6,18 @@ export default function Home() {
   return (
     <main className='overflow-hidden'>
       <Hero />
+      <div className="mt-12 sm:px-16 px-6  py-4 max-w-[1440px] mx-auto">
+        <div className="flex flex-col items-start justify-start gap-y-2.5 text-black-100">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="flex-wrap items-center w-full gap-5 mt-12 flex-between">
+          <SearchBar />
+        </div>
+        <div className="flex flex-wrap items-center justify-start gap-2">
+
+        </div>
+      </div>
     </main>
   )
 }
