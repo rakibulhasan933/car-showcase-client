67a63813a8945bf0599bb4382aaa4cba406f7d82
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-23 02:42:11 +0600

    [ADD] hero final

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ function Hero() {
 		console.log('On click working');
 	}
 	return (
-		<div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
+		<div className="flex xl:flex-row flex-col justify-between gap-5 relative z-0 max-w-[1440px] mx-auto">
 			<div className="flex-1 px-6 pt-36 sm:px-16">
 				<h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
 					Find , Book or rent a Car Quickly and easily!
@@ -22,11 +22,11 @@ function Hero() {
 					handleClick={handleScroll}
 				/>
 			</div>
-			<div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
+			<div className="xl:flex-[1.5] flex items-end w-full xl:h-screen">
 				<div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0">
 					<Image src='/hero.png' alt="Hero" fill className="object-contain " />
-					<div className="absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden" />
 				</div>
+				<div className="absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden"></div>
 			</div>
 		</div>
 	)
