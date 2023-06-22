839c72f11e11e2c0c9c5e8ab1b91e87638117a6d
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-23 01:50:04 +0600

    [ADD] footer careated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Footer() {
 	return (
-		<div>Footer</div>
+		<footer className="flex flex-col mt-5 border-b border-gray-100 text-black-100">
+			<div className="flex flex-wrap justify-between gap-5 px-6 py-10 max-md:flex-col sm:px-16">
+				<div className="flex flex-col items-start justify-start gap-6 ">
+					<Image src="/logo.svg" alt='logo' width={118} height={18} className='object-contain ' />
+					<p className="text-base text-gray-700 "> carHub 2023 <br />All rights reserved &copy; </p>
+				</div>
+			</div>
+		</footer>
 	)
 }
 
