6a487402535b54e5fcc2908e106e1ccd507ac6cb
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-24 01:45:40 +0600

    [ADD] image show in cart

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,5 +1,6 @@
 import { CarProps } from '@/types'
 import { calculateCarRent } from '@/utils'
+import Image from 'next/image';
 import React from 'react'
 
 interface CarCardProps {
@@ -11,7 +12,7 @@ function CarCard({ car }: CarCardProps) {
 	const carRent = calculateCarRent(car.city_mpg, car.year);
 
 	return (
-		<div className='flex flex-col items-start justify-center p-6 text-black-100 bg-primary-blue-100 hover:bg-white hover:shadow-md rounded-3xl group-[]:'>
+		<div className='flex flex-col items-start justify-center p-6 text-black-100 bg-primary-blue-100 hover:bg-white hover:shadow-md rounded-3xl cursor-pointer group-[]:'>
 			<div className="flex items-start justify-between w-full gap-2 ">
 				<h2 className="text-[22px] leading-[26px] font-bold capitalize">
 					{car.make} {car.model}
@@ -26,7 +27,9 @@ function CarCard({ car }: CarCardProps) {
 					/day
 				</span>
 			</p>
-
+			<div className="relative object-contain w-full h-40 my-3 ">
+				<Image src="/hero.png" alt={car.model} fill priority className='object-contain ' />
+			</div>
 		</div>
 	)
 }
