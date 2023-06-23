aa46b385cef4810c86ab2fe7da3dfa000f3c1953
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-23 23:58:42 +0600

    [ADD] data loaded

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ import { fetchCars } from "@/utils";
 
 
 export default async function Home() {
-  const data = await fetchCars();
-  console.log(data);
+  const allCars = await fetchCars();
+
+  console.log(allCars);
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className='overflow-hidden'>
       <Hero />
@@ -21,6 +24,18 @@ export default async function Home() {
           <CustomFilter />
         </div>
       </div>
+      {isDataEmpty ? (
+        <section>
+          <div className="grid w-full grid-cols-1 gap-8 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 pt-14">
+
+          </div>
+        </section>
+      ) : (
+        <div className="flex flex-col items-center justify-center gap-2 mt-16">
+          <h2 className="text-xl font-bold text-black ">Oops, no Results</h2>
+        </div>
+      )
+      }
     </main>
   )
 }
