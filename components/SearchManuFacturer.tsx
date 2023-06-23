80c079212fed121600a53f9163271be8ef2bcc76
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-23 16:21:25 +0600

    [ADD] search bar added

--- components/SearchManuFacturer.tsx
+++ components/SearchManuFacturer.tsx
@@ -1,10 +1,27 @@
+"use client"
 import { SearchManuFacturerProps } from '@/types'
-import React from 'react'
+import { Combobox } from '@headlessui/react'
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 function SearchManuFacturer({ manufacturer, setManufacturer }: SearchManuFacturerProps) {
-	console.log(manufacturer);
+	const [query, setQuery] = useState('');
 	return (
-		<div>SearchManuFacturer</div>
+		<div className="flex items-center justify-start flex-1 max-sm:w-full">
+			<Combobox>
+				<div className="relative w-full">
+					<Combobox.Button className=" absolute top-[14px]">
+						<Image src="/car-logo.svg" alt='car logo' width={20} height={20} />
+					</Combobox.Button>
+					<Combobox.Input className="w-full h-[48px] pl-12 p-4 rounded-l-full max-sm:rounded-full bg-light-white outline-none cursor-pointer text-sm" placeholder='Volkswagen'
+						displayValue={(manufacturer: string) => manufacturer}
+						onChange={(e) => setQuery(e.target.value)}
+					>
+
+					</Combobox.Input>
+				</div>
+			</Combobox>
+		</div>
 	)
 }
 
