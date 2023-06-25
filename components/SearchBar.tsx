c0e46a6d481197197ca4f33dcfa6c16073e8d14f
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-25 13:15:16 +0600

    [ADD] search bar icon added

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,11 +3,16 @@ import React, { useState } from 'react'
 import { SearchManuFacturer } from './'
 import Image from 'next/image';
 
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+	<button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+		<Image src="/public/magnifying-glass.svg" alt='magnifying' width={40} height={40} className='object-contain' />
+	</button>
+)
+
 function SearchBar() {
 	const handleSearch = () => {
 		console.log("search working");
 	}
-
 
 	const [manufacturer, setManufacturer] = useState('');
 	return (
@@ -16,18 +21,10 @@ function SearchBar() {
 				manufacturer={manufacturer}
 				setManufacturer={setManufacturer}
 			/>
-
+			<SearchButton otherClasses="sm:hidden" />
 		</form>
 	)
 }
 
 export default SearchBar;
 
-
-function SearchBarButton({ otherClasses }: { otherClasses: string }) {
-	return (
-		<button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
-			<Image src="/public/magnifying-glass.svg" alt='magnifying' width={40} height={40} className='object-contain' />
-		</button>
-	)
-}
